2b9e1f68c2818fd7e2809c0c13912b6dd6bb1398
Jacob Isbell <jwisbell.astro@gmail.com>
2024-12-20 11:20:01 -0700

    tweaking components

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -4,8 +4,7 @@ import * as Component from "./quartz/components"
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [
-  ],
+  header: Component.Explorer(),
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jwisbell/",
@@ -31,8 +30,8 @@ export const defaultContentPageLayout: PageLayout = {
   ],
   right: [
     Component.DesktopOnly(Component.TableOfContents()),
-    Component.DesktopOnly(Component.Graph())
-    Component.Backlinks(),
+    Component.DesktopOnly(Component.Graph()),
+    //Component.Backlinks(),
   ],
 }
 
